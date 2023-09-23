553e3473166752ac5557536a17d77503c224528c
wagnerparnoff <wagnerparnoffpereira@gmail.com>
2023-09-22 22:23:47 -0300

    todolist

---
+++ backend/src/controllers/tasksControllers.js
@@ -0,0 +1,32 @@
+const tasksModel = require("../models/tasksModel");
+
+const getAll = async (_request, response) => {
+  const tasks = await tasksModel.getAll();
+  return response.status(200).json(tasks);
+};
+
+const createTask = async (request, response) => {
+  const createdTask = await tasksModel.createTask(request.body);
+  return response.status(201).json(createdTask);
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  await tasksModel.deleteTask(id);
+  return res.status(204).json();
+}
+
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+
+  await tasksModel.updateTask(id, req.body);
+  return res.status(204).json();
+};
+
+module.exports = {
+  getAll,
+  createTask,
+  deleteTask,
+  updateTask,
+};
---
+++ backend/src/router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksControllers');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
+const router = express.Router();
+
+
+router.get('/tasks', tasksController.getAll);
+router.post("/tasks", tasksMiddleware.validateFieldTitle, tasksController.createTask);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put("/tasks/:id",
+    tasksMiddleware.validateFieldTitle,
+    tasksMiddleware.validateFieldStatus,
+    tasksController.updateTask
+);
+
+
+module.exports = router
